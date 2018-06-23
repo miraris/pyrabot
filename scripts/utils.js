ba1ca4506ba1afcfa4526a62078730595cb6d86b
miraris <miirariis@gmail.com>
2018-06-23 22:29:18 +0200

    Refactor utils, add an npm start script and more stuff to .gitignore

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,5 +1,3 @@
-var exports = module.exports = {};
-
 const discord = require('discord.js');
 const u = require('./currency.js');
 const s = require('./sentences.js');
@@ -8,50 +6,27 @@ var loaded = new Date().getTime();
 
 console.log("Utils: loading.");
 
-exports.myeval = function(message) {
-    return myeval(message);
+/**
+ * Checks whether the message is from the bot owner
+ *
+ * @param {object} message 
+ * @returns {boolean}
+ */
+const fromOwner = (message) => {
+    return (message.author.id === process.env.OWNER_ID);
 }
 
-exports.save = function(message) {
-    return globalsave(message);
+const scan = (message) => {
+    return fromOwner(message) ? "Not implemented yet" : `You're not ${process.env.OWNER}.`;
 }
 
-exports.botup = function() {
-    return botup();
-}
-
-exports.bothelp = function() {
-    return help();
-}
-
-exports.servers = function(message) {
-    return servers(message);
-}
-
-exports.scan = function(message) {
-    return scan(message);
-}
-
-exports.prettynumber = function(n) {
-    return prettyNumber(n);
-}
-
-exports.timediffstring = function(newest, oldest) {
-    return timedifftostring(newest, oldest);
-}
-
-function scan(message) {
-    if (message.author.id==process.env.OWNER_ID) {
-		return "Not implemented yet";
-    } else return "You're not "+process.env.OWNER+".";
-}
-
-function servers(message) {
-    if (message.author.id==process.env.OWNER_ID) {
+const servers = (message) => {
+    if (fromOwner(message)) {
         let list = message.client.guilds.keyArray();
         let x = "";
-        for (let i=0;i<list.length;i++) {
-            let guild = message.client.guilds.find("id", list[i]);
+        for (let i = 0; i < list.length; i++) {
+            const guild = message.client.guilds.find("id", list[i]);
+
             x += guild.name;
             x += " ("+guild.owner.user.username+")";
             if (i!=list.length-1) x+="; ";
@@ -59,33 +34,33 @@ function servers(message) {
 
         return x;
     }
-    else return "You're not "+process.env.OWNER+".";
+    return `You're not ${process.env.OWNER}.`;
 }
 
-function globalsave(message) {
-    if (message.author.id==process.env.OWNER_ID) {
+const globalsave = (message) => {
+    if (fromOwner(message)) {
         s.save();
         w.save();
         u.save();
         return "Done!";
     }
-    else return "You're not "+process.env.OWNER+".";
+    return `You're not ${process.env.OWNER}.`;
 }
 
-function myeval(message) {
+const myeval = (message) => {
     try {
-        if (message.author.id==process.env.OWNER_ID) {
-            let text = message.content.replace("$secret-eval ","");
+        if (fromOwner(message)) {
+            const text = message.content.replace("$secret-eval ","");
             return eval(text);
         }
-        else return "You're not "+process.env.OWNER+".";
+        return `You're not ${process.env.OWNER}.`;
     } catch (e) {
         console.log("Utils#myeval: Error -> "+e);
         return "An error occured.";
     }
 }
 
-function help() {
+const help = _ => {
     const embed = new discord.RichEmbed()
     .setAuthor("~~ You used the $help command ~~", "https://cdn.discordapp.com/avatars/456934877841981462/5a880bec4e424aab34fcf6f62cc8a363.png?size=128")
     .setColor("#FF0000")
@@ -95,30 +70,31 @@ function help() {
     .addField("$money / $gamble / $gift / $top","Money related stuff. You earn it from chatting. Spamming won't help!")
     .addField("$time","How long I've been up for.");
 
-    return {embed};
+    return { embed };
 }
 
-function timedifftostring(newest, oldest) {
-    let time = newest-oldest;
-    let h = Math.floor(time/(1000*60*60));
-    let m = Math.floor(time/(1000*60))%60;
-    let s = Math.floor(time/(1000))%60;
+const timedifftostring = (newest, oldest) => {
+    const time = newest-oldest;
+    const h = Math.floor(time/(1000*60*60));
+    const m = Math.floor(time/(1000*60))%60;
+    const s = Math.floor(time/(1000))%60;
     let r = "";
-    if (h>0) r += " "+h+" hour";
-    if (h>1) r += "s";
-    if (m>0) r += " "+m+" minute";
-    if (m>1) r += "s";
-    if (s>0) r += " "+s+" second";
-    if (s>1) r += "s";
+    const pluralizeTime = (time, noun, suffix = 's') =>
+    `${time} ${noun}${count !== 1 ? suffix : ''}`;
+
+    r += (h>0) ? pluralizeTime(h, "hour") : '';
+    r += (m>0) ? pluralizeTime(m, "minute") : '';
+    r += (s>0) ? pluralizeTime(s, "second") : '';
+
     return r;
 }
 
-function botup() {
-    return "I've been up for"+timedifftostring(new Date().getTime(), loaded)+".";
+const botup = _ => {
+    return `I've been up for ${timedifftostring(new Date().getTime(), loaded)}.`;
 }
 
-function prettyNumber(n) {
-    let s = n.toString();
+const prettyNumber = (n) => {
+    const s = n.toString();
     let final = "";
     for (let i=0;i<s.length;i++) {
         final += s[i];
@@ -127,4 +103,13 @@ function prettyNumber(n) {
     return final;
 }
 
-
+module.exports = {
+    myeval,
+    save: globalsave,
+    botup,
+    bothelp: help,
+    servers,
+    scan,
+    prettynumber: prettyNumber,
+    timediffstring: timedifftostring
+}
